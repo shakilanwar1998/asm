97e119964a5c30d147931d4f0add4521221f1918
Shakil Anwar <shakilanwar1998@gmail.com>
2024-05-11 09:09:39 +0600

    Confetti

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     </head>
     <body>
         <div id="app"></div>
+{{--        <script src="{{ asset('js/confetti.min.js') }}"></script>--}}
+        <script src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js"></script>
         @vite(['resources/css/app.css','resources/js/app.js'])
     </body>
 </html>
