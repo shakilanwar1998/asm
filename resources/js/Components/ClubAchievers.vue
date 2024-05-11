c39154734b3c979fde349d96512749eae4255e10
Shakil Anwar <shakilanwar1998@gmail.com>
2024-05-11 09:36:25 +0600

    Z index issue

--- resources/js/Components/ClubAchievers.vue
+++ resources/js/Components/ClubAchievers.vue
@@ -45,51 +45,52 @@ export default {
             .then(data => {
                 this.goldTitle = data.goldTitle;
                 this.diamondTitle = data.diamondTitle;
-                this.goldBackground = data.goldBackground;
-                this.diamondBackground = data.diamondBackground;
                 this.tenMillions = data.tenMillions;
                 this.fifteenMillions = data.fifteenMillions;
                 this.goldLogo = data.goldLogo;
                 this.diamondLogo = data.diamondLogo;
-                this.triggerConfetti();
+                this.startConfettiAnimation();
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
     },
     methods: {
-        triggerConfetti() {
-            const defaults = {
-                spread: 360,
-                ticks: 50,
-                gravity: 0,
-                decay: 0.94,
-                startVelocity: 30,
-                shapes: ["star"],
-                colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
-            };
+        startConfettiAnimation() {
+            const duration = 15 * 1000,
+                animationEnd = Date.now() + duration,
+                defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 99 };
 
-            function shoot() {
-                confetti({
-                    ...defaults,
-                    particleCount: 40,
-                    scalar: 1.2,
-                    shapes: ["star"],
-                });
-
-                confetti({
-                    ...defaults,
-                    particleCount: 10,
-                    scalar: 0.75,
-                    shapes: ["circle"],
-                });
+            function randomInRange(min, max) {
+                return Math.random() * (max - min) + min;
             }
 
-            setTimeout(shoot, 0);
-            setTimeout(shoot, 100);
-            setTimeout(shoot, 200);
+            const interval = setInterval(function() {
+                const timeLeft = animationEnd - Date.now();
+
+                if (timeLeft <= 0) {
+                    return clearInterval(interval);
+                }
+
+                const particleCount = 50 * (timeLeft / duration);
+
+                // since particles fall down, start a bit higher than random
+                confetti(
+                    Object.assign({}, defaults, {
+                        particleCount,
+                        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+                    })
+                );
+                confetti(
+                    Object.assign({}, defaults, {
+                        particleCount,
+                        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+                    })
+                );
+            }, 250);
         }
     }
 };
+
 
 </script>
